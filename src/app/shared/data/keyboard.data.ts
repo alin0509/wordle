8a22ae7464b83f637549a78babfa6c56ff84d6f8
frunza alin <frunza_alin@yahoo.com>
2022-03-06 20:25:16 +0200

    Create board components, and word constructing logic (#1)
    * Create board components, and word constructing logic
    
    * Working game + save current status to cookies
    
    * cleanup
    
    * Cleanup and renaming
    
    * add buidl docs
    
    Co-authored-by: Alin <alin.frunza@arnia.ro>

---
+++ src/app/shared/data/keyboard.data.ts
@@ -0,0 +1,38 @@
+import { LetterState } from '../interfaces/index';
+
+export const KeyboardDefaultValues: any[] = [
+    [
+        { value: 'q', state: LetterState.Default },
+        { value: 'w', state: LetterState.Default },
+        { value: 'e', state: LetterState.Default },
+        { value: 'r', state: LetterState.Default },
+        { value: 't', state: LetterState.Default },
+        { value: 'y', state: LetterState.Default },
+        { value: 'u', state: LetterState.Default },
+        { value: 'i', state: LetterState.Default },
+        { value: 'o', state: LetterState.Default },
+        { value: 'p', state: LetterState.Default },
+    ],
+    [
+        { value: 'a', state: LetterState.Default },
+        { value: 's', state: LetterState.Default },
+        { value: 'd', state: LetterState.Default },
+        { value: 'f', state: LetterState.Default },
+        { value: 'g', state: LetterState.Default },
+        { value: 'h', state: LetterState.Default },
+        { value: 'j', state: LetterState.Default },
+        { value: 'k', state: LetterState.Default },
+        { value: 'l', state: LetterState.Default },
+    ],
+    [
+        { value: 'enter', state: LetterState.Default },
+        { value: 'z', state: LetterState.Default },
+        { value: 'x', state: LetterState.Default },
+        { value: 'c', state: LetterState.Default },
+        { value: 'v', state: LetterState.Default },
+        { value: 'b', state: LetterState.Default },
+        { value: 'n', state: LetterState.Default },
+        { value: 'm', state: LetterState.Default },
+        { value: 'back', state: LetterState.Default },
+    ]
+];
---
+++ src/app/shared/index.ts
@@ -0,0 +1,4 @@
+export * from './data/index';
+export * from './helpers/index';
+export * from './services/index';
+export * from './interfaces/index';
